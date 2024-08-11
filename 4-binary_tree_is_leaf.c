11c373f152a47217b3f7af71a319747a6860db7e
“Tristian <tristian.davis@atlasschool.com>
2024-08-11 17:21:05 -0500

    betty didnt like my newline

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -12,7 +12,6 @@ int binary_tree_is_leaf(const binary_tree_t *node)
 	{
 		return (0);
 	}
-	
 	if (node->left != NULL || node->right != NULL)
 	/*if node has a left or right then that means it is not a leaf*/
 	{
