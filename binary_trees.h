5e7108115c964cdd0a70439ed8dcd48e97b69135
“Tristian <tristian.davis@atlasschool.com>
2024-08-11 15:43:57 -0500

    binary tree print

--- binary_trees.h
+++ binary_trees.h
@@ -22,6 +22,27 @@ struct binary_tree_s
     struct binary_tree_s *right;
 };
 
+#include <stddef.h>
+
+/**
+ * struct binary_tree_s - Binary tree node
+ *
+ * @n: Integer stored in the node
+ * @parent: Pointer to the parent node
+ * @left: Pointer to the left child node
+ * @right: Pointer to the right child node
+ */
+typedef struct binary_tree_s
+{
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
+} binary_tree_t;
+
+void binary_tree_print(const binary_tree_t *);
+
+
 typedef struct binary_tree_s binary_tree_t;
 
 typedef struct binary_tree_s bst_t;
@@ -49,9 +70,6 @@ int binary_tree_is_full(const binary_tree_t *tree);
 int binary_tree_is_perfect(const binary_tree_t *tree);
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
-
-
-
 
 
 #endif
