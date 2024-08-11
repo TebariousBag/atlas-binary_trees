f0a04a41b20d45651dba1c1c52c0c1153fd98748
“Tristian <tristian.davis@atlasschool.com>
2024-08-11 17:02:31 -0500

    ==

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -8,12 +8,13 @@
 
 void binary_tree_delete(binary_tree_t *tree)
 {
-	if (tree != NULL)
+	if (tree == NULL)
 	{
 		return;
 	}
 
 	binary_tree_delete(tree->right); /*gonna free left and right*/
 	binary_tree_delete(tree->left);
+
 	free(tree);
 }
