2eb2e94321c2609d9dc8ac8b68dbf8934b6dcb23
“Tristian <tristian.davis@atlasschool.com>
2024-08-12 18:22:58 -0500

    iinorder

---
+++ 7-binary_tree_inorder.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_inorder - inorder traversal
+ * @tree: pointer to root node of tree
+ * @func: pointer to function call for each node
+ */
+
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree == NULL || func == NULL)
+	{
+		return;
+	}
+
+	binary_tree_preorder(tree->left, func); /*traverse left*/
+	func(tree->n); /*then go to root*/
+	binary_tree_preorder(tree->right, func); /*travers right*/
+}
