563315a8edeb28cbd4e62849133faf8593b6bfcb
“Tristian <tristian.davis@atlasschool.com>
2024-08-12 18:07:12 -0500

    preorder

---
+++ 6-binary_tree_preorder.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_preorder - preorder traversal
+ * @tree: pointer to root node of tree
+ * @func: pointer to function call for each node
+ */
+
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
+{
+	if (tree == NULL || func == NULL)
+	{
+		return;
+	}
+
+	func(tree->n);
+	binary_tree_preorder(tree->left, func); /*traverse left*/
+	binary_tree_preorder(tree->right, func); /*travers right*/
+}
