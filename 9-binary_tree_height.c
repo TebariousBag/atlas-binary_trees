2f8dc9e2b9715cd619fa13d9bfaafeff80f46da6
“Tristian <tristian.davis@atlasschool.com>
2024-08-12 19:04:11 -0500

    betty

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,10 +1,10 @@
 #include "binary_trees.h"
 
 /**
- * 
- * 
- * 
- */
+* binary_tree_height - measures height of tree
+* @tree: pointer to root
+* Return: height or 0 if tree is NULL
+*/
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
@@ -15,7 +15,6 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	{
 		return (0);
 	}
-
 	hl = tree->left ? 1 + binary_tree_height(tree->left) : 0;
 	hr = tree->right ? 1 + binary_tree_height(tree->right) : 0;
 
