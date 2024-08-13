057eb7aeba0ec050b7c30ff28a2dbb28fa39bfa0
“Tristian <tristian.davis@atlasschool.com>
2024-08-12 19:02:20 -0500

    testing

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -11,11 +11,14 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	size_t hl = 0; /*height left*/
 	size_t hr = 0; /*height right*/
 
-	if (tree == NULL || (tree->left == NULL && tree->right == NULL))
+	if (!tree)
 	{
 		return (0);
 	}
 
-	return ((hl > hr) ? hl : hr) + 1;
+	hl = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+	hr = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+
+	return (hl > hr ? hl : hr);
 
 }
